1f1739001e7540ab1d72c87a2abfa2c6024abec1
abfgry@gmail.com <abfgry@gmail.com@303da5fe-9dbd-5584-5757-57e33a95b0d8>
2011-05-05 17:32:24 +0000

    Adding server base code

---
+++ trunk/server/server.cpp
@@ -0,0 +1,260 @@
+//Brent Kapral Multi-threaded server
+//Modified code from mycourses download
+
+#include <windows.h>
+#include  <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <winsock.h>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+// our thread for recving commands
+DWORD WINAPI receive_cmds(LPVOID lpParam)
+{
+  printf("thread created\r\n");
+  
+  
+  
+  // set our socket to the socket passed in as a parameter 
+  SOCKET current_client = (SOCKET)lpParam;
+	//Create new socket address struct
+	sockaddr_in cAddress;
+	//get length of socket address struct
+	int len=sizeof(cAddress);
+	//populate socket address struct for IP address later
+	getpeername(current_client, (SOCKADDR*)&cAddress, &len);
+  // buffer to hold our recived data
+  char rcvbuf[100];
+  // buffer to hold our sent data
+  char sendData[100];
+  // for error checking
+  int res;
+   
+   
+   //LOGGING BEGIN
+   
+   
+   //Get the current thread ID for logging
+  int threadID = GetCurrentThreadId();
+  
+  //open master log file
+  ofstream fout;
+  fout.open("connection_log.csv", fstream::app);
+  
+  
+  //Log the time of the connection
+  	time_t seconds;
+	struct tm * timeinfo;
+	time(&seconds);
+	timeinfo = localtime (&seconds);
+	     
+    //output "mm/dd/yyyy",
+    fout << "\"" << timeinfo->tm_mon + 1 << "/" << timeinfo->tm_mday << "/" << timeinfo->tm_year + 1900 << "\"," ;
+	//output "time hh:mm:ss"
+	fout << "\"" << timeinfo->tm_hour << ":" << timeinfo->tm_min << ":" << timeinfo->tm_sec << "\",";
+	//Output connection attempt
+	fout << "\"" << "Thread ID:\"," << "\"" << threadID << "\",";
+	fout << "\"" << "Client IP Address:\"," << "\"" << inet_ntoa(cAddress.sin_addr) << "\"\n";
+	fout.close();
+  
+  
+  //Create the name for the output file
+  char messageLog[32];
+  char ThrdID[16];
+  strcpy(messageLog, "message_log_");
+  strcat(messageLog, itoa(threadID, ThrdID, 10));
+  strcat(messageLog, ".csv");
+  
+  cout << "Message log name for thread " << threadID << ": " << messageLog << endl;
+  fout.open(messageLog);
+  
+  
+  // strcpy(sendData,"hello little friend\n");
+  // cout << strlen(sendData) << "\n";
+  // send(current_client,sendData,strlen(sendData),0);
+   
+  // our recv loop
+  while(true)
+  {
+	 
+     res = recv(current_client,rcvbuf,sizeof(rcvbuf),0); // recv cmds
+     cout << "strlen is: " << strlen(rcvbuf) << endl;
+     cout << "sizeof is: " << sizeof(rcvbuf) << endl;
+	 char rcvmsg[(strlen(rcvbuf)-1)];//CREATE NEW BUFFER WITH SIZE OF MESSAGE RECIEVED MINUS 1
+	 strcpy(rcvmsg, ""); //EMPTY "RECIEVED MESSAGE"
+	//FILL RECIEVED MESSAGE WITH RECIEVED BUFFER
+	 strcpy(rcvmsg, rcvbuf);
+	 int endln = strlen(rcvmsg);
+	 rcvmsg[endln-1] = '\0';
+	 cout << "strlen is: " << strlen(rcvmsg) << endl;
+     cout << "sizeof is: " << sizeof(rcvmsg) << endl;
+     cout << "res is: " << res << endl;
+     
+	for (int i = 0; i < strlen(rcvmsg); i++)
+	{
+		cout << rcvmsg[i];// << endl;
+	}
+     
+	 //what did I receive
+     cout << "Received:" << rcvmsg << endl;// << ":stuff" << "\n";
+     
+     //LOG TIME AND MESSAGE RECEIVED
+     //output "mm/dd/yyyy",
+    fout << "\"" << timeinfo->tm_mon + 1 << "/" << timeinfo->tm_mday << "/" << timeinfo->tm_year + 1900 << "\"," ;
+	//output "time hh:mm:ss"
+	fout << "\"" << timeinfo->tm_hour << ":" << timeinfo->tm_min << ":" << timeinfo->tm_sec << "\",";
+	//output other data
+	fout << "\"" << "Thread ID:\"," << "\"" << threadID << "\",";
+	fout << "\"" << "Client IP Address:\"," << "\"" << inet_ntoa(cAddress.sin_addr) << "\",";
+	fout << "\"" << "Received Message: \"," << "\"" << rcvmsg << "\",";
+     
+
+     if (res != 0)
+     {
+ 
+          /* Put your stuff here */
+        
+        //create copy of rcvbuf in order to change it to lower case for comparison
+		char rcvcopy[sizeof(rcvmsg)];
+		strcpy(rcvcopy,rcvmsg);
+        
+          //copy to lowercase string
+        for(int i = 0; i < sizeof(rcvcopy); i++)
+        {
+			rcvcopy[i]=tolower(rcvcopy[i]);
+		}
+		
+		//cout << "rcvbuf is: " << rcvbuf << endl; //DEBUGGING
+	//	cout << "rcvcopy is: " << rcvcopy << endl;//DEBUGGING
+		
+		//check if client says hello or a varient thereof
+        if((strcmp(rcvcopy,"hello") == 0) || (strcmp(rcvcopy,"helo") == 0) || (strcmp(rcvcopy,"ehlo") == 0))
+		{		
+		
+			cout << "Client ip address is: " << inet_ntoa(cAddress.sin_addr) << endl;
+			//create the string to send back to the client
+			strcpy(sendData, "Hello there ");
+			strcat(sendData, inet_ntoa(cAddress.sin_addr));
+			strcat(sendData, ". Thanks for joining my server.");
+			send(current_client,sendData,sizeof(sendData),0);
+			fout << "\"" << "Sent Message: \",\"" << sendData << "\"\n";
+			//Empty the send buffer
+			for (int i = 0; i <100; i++)
+			{
+				sendData[i] = '\0';
+			}
+			
+			
+		}//If client says bye or end or quit, close the thread.
+		else if((strcmp(rcvcopy,"bye")==0) || (strcmp(rcvcopy,"end")==0) || (strcmp(rcvcopy,"quit") == 0)|| (strcmp(rcvcopy,"exit") == 0))
+		{
+			strcpy(sendData,"Thank You, Please come again.");
+			fout << "\"" << "Sent Message: \",\"" << sendData << "\"\n";
+			send(current_client,sendData,sizeof(sendData),0);
+			fout.close();
+			ExitThread(0);
+			
+		
+		}//Otherwise, echo what the client said back to them
+		else
+		{
+		//	cout << "send data is: " << sendData << endl;
+		//	cout << "rcvbuf is: " << rcvbuf << endl;
+			strcpy(sendData,"");
+			strcpy(sendData,rcvmsg);
+			strcpy(rcvmsg,"");
+			fout << "\"" << "Sent Message: \",\"" << sendData << "\"\n";
+			send(current_client,sendData,sizeof(sendData),0);
+		} 
+          
+          
+          
+          
+          /* Stop putting your stuff here */  
+                              
+      
+     // clear buffers
+       strcpy(sendData,"");
+       strcpy(rcvbuf,"");
+       strcpy(rcvcopy,"");
+      // cout << "at end, sendData, rcvbuf, rcvcpy: " << sendData << " , " << rcvbuf << " , " << rcvcopy << endl; //DEBUGGING
+  	}
+  }  
+}
+ 
+int main()
+{
+ printf("Starting up multi-threaded TCP server\n");
+ //seed random number generator
+ srand(time(NULL));
+
+ SOCKET sock;
+  
+ // for our thread
+ DWORD thread;
+  
+ WSADATA wsaData;
+ sockaddr_in server;
+ 
+  
+ // start winsock
+ int ret = WSAStartup(0x101,&wsaData); // use highest version of winsock avalible
+  
+ if(ret != 0)
+ {
+    return 0;
+ }
+   
+ // fill in winsock struct ...
+ server.sin_family=AF_INET;
+ server.sin_addr.s_addr=INADDR_ANY;
+ server.sin_port=htons(31000); // listen on telnet port 31000
+  
+ // create our socket
+ sock=socket(AF_INET,SOCK_STREAM,0);
+  
+ if(sock == INVALID_SOCKET)
+ {
+    return 0;
+ }
+   
+ // bind our socket to a port(port 123)
+ if( bind(sock,(sockaddr*)&server,sizeof(server)) !=0 )
+ {
+    return 0;
+ }
+   
+ // listen for a connection 
+ if(listen(sock,5) != 0)
+ {
+    return 0;
+ }
+  
+ // socket that we sendrecv data on
+ SOCKET client;
+  
+ sockaddr_in from;
+ int fromlen = sizeof(from);
+   
+ // loop forever
+ while(true)
+ {
+  // accept connections
+  client = accept(sock,(struct sockaddr*)&from,&fromlen);
+  printf("Client connected\r\n");
+   
+  // create our recv_cmds thread and parse client socket as a parameter
+  CreateThread(NULL, 0,receive_cmds,(LPVOID)client, 0, &thread); 
+ }
+  
+ // shutdown winsock
+ closesocket(sock);
+ WSACleanup();
+  
+ // exit
+ return 0;
+}
