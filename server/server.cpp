09ad2c7ea426a27b5487d9e2b7ce2a10c5fe9b24
abfgry@gmail.com <abfgry@gmail.com@303da5fe-9dbd-5584-5757-57e33a95b0d8>
2011-05-09 15:17:51 +0000

    Made changes to server/server.cpp  currently non-working

--- server/server.cpp
+++ server/server.cpp
@@ -6,8 +6,15 @@
 #include <stdio.h>
 #include <iostream>
 #include <winsock.h>
+#include <winsock2.h>
 #include <string>
 #include <fstream>
+#include <fnctl.h>
+#pragma comment(lib, "Ws2_32.lib")
+#define SERVER_PORT 25
+#define LSIDE 
+#define RSIDE
+#define OUR_DOMAIN "droptables.com"
 
 
 using namespace std;
@@ -33,7 +40,10 @@ DWORD WINAPI receive_cmds(LPVOID lpParam)
   char sendData[100];
   // for error checking
   int res;
-   
+  int flags = fcntl(current_client, F_GETFL);
+  int result = fcntl(current_client, F_SETFL, flags & ~O_NONBLOCK);
+
+
    
    //LOGGING BEGIN
    
@@ -76,7 +86,7 @@ DWORD WINAPI receive_cmds(LPVOID lpParam)
   strcpy(sendData,"220 smtp.droptables.com\n");
   //cout << strlen(sendData) << "\n";
   send(current_client,sendData,strlen(sendData),0);
-   
+   fout << inet_ntoa(cAddress.sin_addr);
   // our recv loop
   while(true)
   {
@@ -88,91 +98,139 @@ DWORD WINAPI receive_cmds(LPVOID lpParam)
 	 strcpy(rcvmsg, rcvbuf);
 	 int endln = strlen(rcvmsg);
 	 rcvmsg[endln-1] = '\0';//APPEND A NULL TO THE END OF THE BUFFER
-     
-     
+	 
 	 //what did I receive
+	 fout << endl << rcvmsg;//Output to file all data
      cout << "Received:" << rcvmsg << endl;// << ":stuff" << "\n";
      
-     //LOG TIME AND MESSAGE RECEIVED
-     //output "mm/dd/yyyy",
-    fout << "\"" << timeinfo->tm_mon + 1 << "/" << timeinfo->tm_mday << "/" << timeinfo->tm_year + 1900 << "\"," ;
-	//output "time hh:mm:ss"
-	fout << "\"" << timeinfo->tm_hour << ":" << timeinfo->tm_min << ":" << timeinfo->tm_sec << "\",";
-	//output other data
-	fout << "\"" << "Thread ID:\"," << "\"" << threadID << "\",";
-	fout << "\"" << "Client IP Address:\"," << "\"" << inet_ntoa(cAddress.sin_addr) << "\",";
-	fout << "\"" << "Received Message: \"," << "\"" << rcvmsg << "\",";
-     
-
-     if (res != 0)
+    	
+	 if (res != 0)
      {
- 
-        //create copy of rcvbuf in order to change it to lower case for comparison
-		char rcvcopy[sizeof(rcvmsg)];
-		strcpy(rcvcopy,rcvmsg);
-        
-          //copy to lowercase string
-        for(int i = 0; i < sizeof(rcvcopy); i++)
-        {
-			rcvcopy[i]=tolower(rcvcopy[i]);
-		}
-		
-		
-		
-		//check if client says hello or a varient thereof
-        if((strcmp(rcvcopy,"hello") == 0) || (strcmp(rcvcopy,"helo") == 0) || (strcmp(rcvcopy,"ehlo") == 0))
+		if((strncmp(rcvmsg,"HELO",4) == 0) || (strncmp(rcvmsg,"EHLO",4) == 0))
 		{		
-		
+			
 			cout << "Initialized connection from: " << inet_ntoa(cAddress.sin_addr) << endl;
 			//create the string to send back to the client
 			strcpy(sendData, "250  ");
-			int len1 = strlen(rcvmsg);
+			//int len1 = strlen(rcvmsg);
 			
-			strncat(sendData, rcvmsg, (len1-2));
-		//	strcat(sendData, inet_ntoa(cAddress.sin_addr));
+			strncat(sendData, rcvmsg, (strlen(rcvmsg)));
+	
 			strcat(sendData, ". Thanks for joining my server.");
 			send(current_client,sendData,sizeof(sendData),0);
-			fout << "\"" << "Sent Message: \",\"" << sendData << "\"\n";
+		
 			//Empty the send buffer
 			for (int i = 0; i <100; i++)
 			{
 				sendData[i] = '\0';
 			}
 			
-			
-		}//If client says bye or end or quit, close the thread.
-		else if((strcmp(rcvcopy,"quit") == 0))
+		}
+		if((strncmp(rcvmsg,"MAIL FROM:",10) == 0) || (strncmp(rcvmsg,"RCPT TO:",8) == 0))
+		{
+			strncpy(sendData,"250 Ok\0",7);
+			send(current_client,sendData,sizeof(sendData),0);
+		}
+
+			if((strcmp(rcvmsg,".") == 0))
+			{
+				strncpy(sendData,"250 Ok:",7);
+				strcat(sendData, " Queued as: ");
+				strcat(sendData, itoa(threadID, ThrdID, 10));
+				send(current_client,sendData,sizeof(sendData),0);
+			}
+			if((strcmp(rcvmsg,"QUIT") == 0))
 		{
 			strcpy(sendData,"221 Bye");
-			fout << "\"" << "Sent Message: \",\"" << sendData << "\"\n";
 			send(current_client,sendData,sizeof(sendData),0);
 			fout.close();
 			ExitThread(0);
-			
-		
-		}//Otherwise, echo what the client said back to them
-		else
+		}
+		if((strcmp(rcvmsg,"DATA") == 0))
 		{
-		
-			strcpy(sendData,"");
-			strcpy(sendData,rcvmsg);
-			strcpy(rcvmsg,"");
-			fout << "\"" << "Sent Message: \",\"" << sendData << "\"\n";
+			strcpy(sendData,"354 End data with <CR><LF>.<CR><LF>");
 			send(current_client,sendData,sizeof(sendData),0);
-		} 
-          
-          
-          
-          
-          /* Stop putting your stuff here */  
-                              
-      
-     // clear buffers
-       strcpy(sendData,"");
-       strcpy(rcvbuf,"");
-       strcpy(rcvcopy,"");
-      // cout << "at end, sendData, rcvbuf, rcvcpy: " << sendData << " , " << rcvbuf << " , " << rcvcopy << endl; //DEBUGGING
-  	}
+			strcpy(sendData,"");
+		send(current_client,sendData,sizeof(sendData),0);
+		}
+	}
+// 		
+//        //create copy of rcvbuf
+//		char rcvcopy[100];
+//		strcpy(rcvcopy,rcvmsg); // create a copy for modification
+//		char fromAddress[strlen(rcvcopy)];//Create a variable to store the from address
+//		char toDomain[strlen(rcvcopy)]; //Create a new char array to store rcpt address	
+//		int rcptCtr = 0;
+//		//check if client says hello or a varient thereof
+//        if((strncmp(rcvcopy,"HELO",4) == 0) || (strncmp(rcvcopy,"EHLO",4) == 0))
+//		{		
+//			
+//			cout << "Initialized connection from: " << inet_ntoa(cAddress.sin_addr) << endl;
+//			//create the string to send back to the client
+//			strcpy(sendData, "250  ");
+//			//int len1 = strlen(rcvmsg);
+//			
+//			strncat(sendData, rcvmsg, (strlen(rcvmsg)));
+//		//	strcat(sendData, inet_ntoa(cAddress.sin_addr));
+//			strcat(sendData, ". Thanks for joining my server.");
+//			send(current_client,sendData,sizeof(sendData),0);
+//		//	fout << sendData << "\n";
+//			//Empty the send buffer
+//			for (int i = 0; i <100; i++)
+//			{
+//				sendData[i] = '\0';
+//			}
+//			
+//		//	strcpy(rcvcopy,rcvmsg);
+//		}//If client says bye or end or quit, close the thread.
+//		else if((strcmp(rcvcopy,"QUIT") == 0))
+//		{
+//			strcpy(sendData,"221 Bye");
+//		//	fout << "\"" << "Sent Message: \",\"" << sendData << "\"\n";
+//			send(current_client,sendData,sizeof(sendData),0);
+//			fout.close();
+//			ExitThread(0);
+//			
+//		
+//		}//Check for MAIL FROM And store the from address into a variable
+//		else if((strncmp(rcvcopy,"MAIL FROM:",10) == 0)){			
+//			strncpy(fromAddress,rcvcopy,strlen(rcvcopy));
+//			strcpy(sendData, "250 Ok");
+//			send(current_client,sendData,sizeof(sendData),0);
+//		}//Check for RCPT TO and see where it is destined for and route it properly
+//		else if((strncmp(rcvcopy,"RCPT TO:",8) == 0))
+//		{
+//			
+//			strcpy(toDomain,(strstr(rcvcopy,"@")+1)); //copy string from char after @ to end
+//			toDomain[(strlen(toDomain))-1]='\0'; //append null char over the last > to terminate the string
+//			if(strcmp(toDomain,OUR_DOMAIN) == 0)
+//			{
+//				
+//			cout << "OUR DOMAIN!!!!" << endl;
+//			
+//			}
+//		}
+//		else{
+//		
+//			strcpy(sendData,"");
+//			strcpy(sendData,rcvmsg);
+//			strcpy(rcvmsg,"");
+//		//	fout << "\"" << "Sent Message: \",\"" << sendData << "\"\n";
+//			send(current_client,sendData,sizeof(sendData),0);
+//		} 
+//          
+//          
+//          
+//          
+//     
+//                              
+//      
+//     // clear buffers
+//       strcpy(sendData,"");
+//       strcpy(rcvbuf,"");
+//       strcpy(rcvcopy,"");
+//      // cout << "at end, sendData, rcvbuf, rcvcpy: " << sendData << " , " << rcvbuf << " , " << rcvcopy << endl; //DEBUGGING
+//  	}
   }  
 }
  
@@ -181,6 +239,7 @@ int main(int argc, char *argv[])
  printf("Starting up multi-threaded TCP server\n");
  //seed random number generator
  srand(time(NULL));
+ u_long iMode = 1;
 
  SOCKET sock;
   
@@ -188,12 +247,16 @@ int main(int argc, char *argv[])
  DWORD thread;
   
  WSADATA wsaData;
+ 
  sockaddr_in server;
  
   
  // start winsock
- int ret = WSAStartup(0x101,&wsaData); // use highest version of winsock avalible
-  
+ int ret = WSAStartup(0x202,&wsaData); // use highest version of winsock avalible
+// int iResult = ioctlsocket(sock, FIONBIO, &iMode);
+//  cout << "iresult: " << iResult << endl;
+// if (iResult != NO_ERROR)
+//  printf("Error at WSAStartup()\n");
  if(ret != 0)
  {
     return 0;
@@ -202,7 +265,7 @@ int main(int argc, char *argv[])
  // fill in winsock struct ...
  server.sin_family=AF_INET;
  server.sin_addr.s_addr=INADDR_ANY;
- server.sin_port=htons(25); // listen on SMTP port 25
+ server.sin_port=htons(SERVER_PORT); // listen on SMTP port 25
   
  // create our socket
  sock=socket(AF_INET,SOCK_STREAM,0);
